ba0389b630bafd5adb243e5e712800673b26d7ee
Nihal Shrivastav <nihalrs24@gmail.com>
2025-02-14 22:29:15 +0530

    Updates

---
+++ app.js
@@ -0,0 +1,9 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".app", {
+        strings: ["App Development"],
+        typeSpeed: 100,
+        backSpeed: 150,
+        loop: true
+    });
+  });
